9f43e652ea4413713db1ce30bb08e49f055aacdb
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-06-19 16:13:26 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -20,7 +20,7 @@ RELEASE_YEAR: int = 2022
 HOMEPAGE_URL: str = "https://universe.roboflow.com/deertracker/deer-yidlt"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 657121
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/deer-yidlt"
